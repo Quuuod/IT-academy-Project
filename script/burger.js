1a88ee9e7b65d020a6864e08e0768899e7382514
QQ <71001778+Quuuod@users.noreply.github.com>
2023-03-29 21:04:06 +0300

    burger, styles, loginform

---
+++ script/burger.js
@@ -0,0 +1,45 @@
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+const navbar = document.querySelector(".navbar")
+const shadow = document.querySelector(".shadow")
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector("#menu").cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  navbar.classList.toggle("opened")
+  shadow.classList.toggle("hidden")
+  renderPopup();
+}
+
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+shadow.addEventListener('click', closeOnClick)
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+  shadow.classList.add('hidden')
+  navbar.classList.remove('opened')
+}
